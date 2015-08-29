7261ae45b5665840b887f01a685d270ddd448564
Nick Janetakis <nick.janetakis@gmail.com>
2015-08-29 17:01:11 -0400

    Fix missing port reference in app.py

--- chapter-7/website/app.py
+++ chapter-7/website/app.py
@@ -10,4 +10,4 @@ def hello_world():
     return 'You visited {0} times!'.format(hits)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
